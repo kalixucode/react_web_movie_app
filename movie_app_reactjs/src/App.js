de50932187cd47ef24de669d9b8517543ca5bebb
sungho_yu <sungho_yu@tmax.co.kr>
2020-09-14 13:07:27 +0900

    react_prototype

--- movie_app_reactjs/src/App.js
+++ movie_app_reactjs/src/App.js
@@ -1,48 +1,62 @@
 import React from 'react';
-
+import PropTypes from "prop-types";
 const foodILike = [
   {
-    id:1,
+    id: 1,
     name: "Kimchi",
     image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 5
   },
   {
-    id:2,
+    id: 2,
     name: "Samgyeopsal",
     image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 4.2
   },
   {
-    id:3,
+    id: 3,
     name: "Bibimbap",
     image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 3.1
   },
   {
-    id:4,
+    id: 4,
     name: "Doncasu",
     image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 4.5
   },
   {
-    id:5,
+    id: 5,
     name: "Kimbap",
     image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 4.1
   }
 ];
 
 
-function Food({name, picture}) {
-  console.log(name);
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h2>I like {name}</h2>
-      <img src= {picture}/>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} />
     </div>
   )
 }
+
+Food.propTypes = {
+  name : PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.number.isRequired,
+}
+//propTypes로 array, boolean, true, false, object etc 체크 가능!
+//isRequired : components가 반드시 있어야하 하는지...없으면 undefined 가능!
+//https://ko.reactjs.org/docs/typechecking-with-proptypes.html
 
 
 // function renderFood(item) {
@@ -51,8 +65,8 @@ function Food({name, picture}) {
 // }
 function App() {
   return (
-    <div>      
-      {foodILike.map(item => <Food key={item.id} name={item.name} picture={item.image}/> )} 
+    <div>
+      {foodILike.map(item => <Food key={item.id} name={item.name} picture={item.image} rating={item.rating} />)}
       {/* {foodILike.map(renderFood)} */}
     </div>
   );

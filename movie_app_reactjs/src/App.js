b7608086dc67928ed45357752b7f5d716e743327
sungho_yu <sungho_yu@tmax.co.kr>
2020-09-15 19:31:17 +0900

    react_Fetching MoviefromAPI&Rendering the Movies&Adding Genres

--- movie_app_reactjs/src/App.js
+++ movie_app_reactjs/src/App.js
@@ -1,20 +1,59 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
     state = {
         isLoading: true,
         movies: []
     };
+    getMovies = async () => {
+        const { data: { data: { movies } } } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+        //movies.data.data.movies => {data: {data: {movies}}} //es6버전
+        // this.setState({ movies: movies }); //movies:movies = state의 movies:data의 movies  =>  movies로 해도 js가 이해한다.
+        // this.setState({movies}); //isLoading도 수정!
+        this.setState({ movies, isLoading: false });
+        console.log(this.state);
+    }
     componentDidMount() { //이론적으로 componentDidMount에서 data를 fetch하는 것!
-        setTimeout(() => {
-            this.setState({ isLoading: false});
-        }, 6000);
+        // setTimeout(() => {d
+        //     this.setState({ isLoading: false});
+        // }, 6000);
+        this.getMovies();
     }
     render() {
-        const { isLoading } = this.state;
-        return <div>{
-            isLoading ? "Loading" : "We are ready"}</div>;
+        const { isLoading, movies } = this.state;
+        return (
+            <section className="container">
+                {
+                    isLoading ? (
+                        // "Loading"
+                        <div className="loader">
+                            <span className="loader_text">Loading...</span>
+                        </div>)
+                        :
+                        // movies.map(movie => {
+                        //     // console.log(movie);
+                        //     return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+
+                        (
+                            <div className="movies">
+                                {movies.map(movie => (
+                                    <Movie
+                                        key={movie.id}
+                                        id={movie.id}
+                                        year={movie.year}
+                                        title={movie.title}
+                                        summary={movie.summary}
+                                        poster={movie.medium_cover_image}
+                                        genres={movie.genres}
+                                    />
+                                ))}
+                            </div>
+                        )
+                }</section>)
     }
 }
-
+//jsx에서는 class대신 className
 export default App;
---
+++ movie_app_reactjs/src/Movie.js
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
+//movies component는 state를 필요로 하지 않는다 => 굳이 class component 사용할 필요 없다.
+
+function Movie({ year, title, summary, poster, genres }) {
+    // return <h4>{title}</h4>;
+    return <div className="movie">
+        <img src={poster} alt={title} title={title} />
+        <div className="movie_data">
+            <h3 className=" movie__title">{title}</h3>
+            <h5 className=" movie__year">{year}</h5>
+            <ul className="genres">
+                {genres.map((genre, index) =>
+                    <li key={index} className="genres__genre">
+                        {genre}
+                    </li>
+                )}
+            </ul>
+            <p className="movie_summary">{summary}</p>
+
+        </div>
+    </div>
+}
+//tag 안에 style={{}} : js css사용 가능
+//ex) <h3 class=" movie__title" style={{backgroundColor: "red"}}>{title}</h3>
+//index를 넘겨야 한다. Why? => key!
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+export default Movie;
+
